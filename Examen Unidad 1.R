fc19aeff463b566d5094be306c8344eb584e4c52
Alejandra11683 <ale78009@gmail.com>
2020-10-25 15:41:46 -0600

    Esta muylargo el trabajo y me costo un poco hacerlo

---
+++ Examen Unidad 1.R
@@ -0,0 +1,9 @@
+datos(flores) 
+# Vamos a explorar la matriz de datos con los siguientes comandos
+? millas  # Ayuda
+dim(millas) # dimensión
+names(millas) # nombre de las variables
+str(millas) # tipo de variable
+anyNA(millas) # me indica los datos perdidos
+typeof(millas) # tipo de matriz de datos
+View(millas) # visualizacion de la tabla (data frame)
